68f183a20282508a7d2644904a269bd99edb33a2
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2023-11-29 13:59:36 +0300

    Feat form for adding as much education as wanted

--- src/CVTemplate.jsx
+++ src/CVTemplate.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import GeneralInfoForm from "./GeneralInfoForm";
+import EducationForm from "./EducationForm";
 
 export default function CVTemplate() {
 	const [generalInfo, setGeneralInfo] = useState({
@@ -9,10 +10,35 @@ export default function CVTemplate() {
 		email: "",
 		phone: "",
 	});
+	const [education, setEducation] = useState([]);
 
 	return (
 		<div className="template">
 			<GeneralInfoForm info={generalInfo} onChange={setGeneralInfo} />
+			<span>Add Education</span>
+			<button
+				type="button"
+				onClick={() =>
+					setEducation([
+						...education,
+						{
+							id: Math.random() * 1000,
+						},
+					])
+				}
+			>
+				+
+			</button>
+			<ul>
+				{education.map((edu) => (
+					<EducationForm
+						education={education}
+						obj={edu}
+						educationId={edu.id}
+						setEducation={setEducation}
+					/>
+				))}
+			</ul>
 		</div>
 	);
 }
---
+++ src/EducationForm.jsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+
+export default function EducationForm({
+	education,
+	obj,
+	educationId,
+	setEducation,
+}) {
+	function handleInstitutionChange(e) {
+		setEducation(
+			education.map((edu) => {
+				if (edu.id === educationId) {
+					return { ...edu, institution: e.target.value };
+				} else {
+					return edu;
+				}
+			})
+		);
+	}
+
+	function handleDegreeChange(e) {
+		setEducation(
+			education.map((edu) => {
+				if (edu.id === educationId) {
+					return { ...edu, degree: e.target.value };
+				} else {
+					return edu;
+				}
+			})
+		);
+	}
+
+	function handleStartYearChange(e) {
+		setEducation(
+			education.map((edu) => {
+				if (edu.id === educationId) {
+					return { ...edu, startYear: e.target.value };
+				} else {
+					return edu;
+				}
+			})
+		);
+	}
+
+	function handleEndYearChange(e) {
+		setEducation(
+			education.map((edu) => {
+				if (edu.id === educationId) {
+					return { ...edu, endYear: e.target.value };
+				} else {
+					return edu;
+				}
+			})
+		);
+	}
+
+	function handleLocationChange(e) {
+		setEducation(
+			education.map((edu) => {
+				if (edu.id === educationId) {
+					return { ...edu, location: e.target.value };
+				} else {
+					return edu;
+				}
+			})
+		);
+	}
+
+	return (
+		<li key={educationId}>
+			<form>
+				<label>
+					Institution:{" "}
+					<input
+						type="text"
+						id="institution"
+						onChange={handleInstitutionChange}
+					/>
+				</label>
+				<label>
+					Degree:{" "}
+					<input type="text" id="degree" onChange={handleDegreeChange} />
+				</label>
+				<label>
+					Start Year:{" "}
+					<input
+						type="number"
+						id="start-year"
+						onChange={handleStartYearChange}
+					/>
+				</label>
+				<label>
+					End Year:{" "}
+					<input type="number" id="end-year" onChange={handleEndYearChange} />
+				</label>
+				<label>
+					Location:{" "}
+					<input type="text" id="location" onChange={handleLocationChange} />
+				</label>
+			</form>
+
+			<button
+				onClick={() =>
+					setEducation(education.filter((edu) => edu.id !== educationId))
+				}
+			>
+				DELETE
+			</button>
+		</li>
+	);
+}
