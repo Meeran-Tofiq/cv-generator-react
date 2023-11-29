2255638fc88fab31f3116ae81465e7a60bad7f0b
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2023-11-29 16:15:49 +0300

    Feat experience form to add how much ever wanted

--- src/CVTemplate.jsx
+++ src/CVTemplate.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import GeneralInfoForm from "./GeneralInfoForm";
 import EducationForm from "./EducationForm";
+import ExperienceForm from "./ExperienceForm";
 
 export default function CVTemplate() {
 	const [generalInfo, setGeneralInfo] = useState({
@@ -11,11 +12,12 @@ export default function CVTemplate() {
 		phone: "",
 	});
 	const [education, setEducation] = useState([]);
+	const [experience, setExperience] = useState([]);
 
 	return (
 		<div className="template">
 			<GeneralInfoForm info={generalInfo} onChange={setGeneralInfo} />
-			<span>Add Education</span>
+			<span>Education</span>
 			<button
 				type="button"
 				onClick={() =>
@@ -33,9 +35,31 @@ export default function CVTemplate() {
 				{education.map((edu) => (
 					<EducationForm
 						education={education}
-						obj={edu}
 						educationId={edu.id}
 						setEducation={setEducation}
+					/>
+				))}
+			</ul>
+			<span>Experience</span>
+			<button
+				type="button"
+				onClick={() =>
+					setExperience([
+						...experience,
+						{
+							id: Math.random() * 1000,
+						},
+					])
+				}
+			>
+				+
+			</button>
+			<ul>
+				{experience.map((exp) => (
+					<ExperienceForm
+						experience={experience}
+						experienceId={exp.id}
+						setExperience={setExperience}
 					/>
 				))}
 			</ul>
---
+++ src/ExperienceForm.jsx
@@ -0,0 +1,116 @@
+export default function ExperienceForm({
+	experience,
+	experienceId,
+	setExperience,
+}) {
+	function handleInstitutionChange(e) {
+		setExperience(
+			experience.map((exp) => {
+				if (exp.id === experienceId) {
+					return { ...exp, institution: e.target.value };
+				} else {
+					return exp;
+				}
+			})
+		);
+	}
+
+	function handlePositionChange(e) {
+		setExperience(
+			experience.map((exp) => {
+				if (exp.id === experienceId) {
+					return { ...exp, position: e.target.value };
+				} else {
+					return exp;
+				}
+			})
+		);
+	}
+
+	function handleStartDateChange(e) {
+		setExperience(
+			experience.map((exp) => {
+				if (exp.id === experienceId) {
+					return { ...exp, startDate: e.target.value };
+				} else {
+					return exp;
+				}
+			})
+		);
+	}
+
+	function handleEndDateChange(e) {
+		setExperience(
+			experience.map((exp) => {
+				if (exp.id === experienceId) {
+					return { ...exp, endDate: e.target.value };
+				} else {
+					return exp;
+				}
+			})
+		);
+	}
+
+	function handleLocationChange(e) {
+		setExperience(
+			experience.map((exp) => {
+				if (exp.id === experienceId) {
+					return { ...exp, location: e.target.value };
+				} else {
+					return exp;
+				}
+			})
+		);
+	}
+
+	function handleDescriptionChange(e) {
+		setExperience(
+			experience.map((exp) => {
+				if (exp.id === experienceId) {
+					return { ...exp, description: e.target.value };
+				} else {
+					return exp;
+				}
+			})
+		);
+	}
+
+	return (
+		<li key={experienceId}>
+			<form>
+				<label>
+					Institution:{" "}
+					<input
+						type="text"
+						id="institution"
+						onChange={handleInstitutionChange}
+					/>
+				</label>
+				<label>
+					Position:{" "}
+					<input type="text" id="position" onChange={handlePositionChange} />
+				</label>
+				<label>
+					Start Date:{" "}
+					<input type="date" id="start-date" onChange={handleStartDateChange} />
+				</label>
+				<label>
+					End Date:{" "}
+					<input type="date" id="end-date" onChange={handleEndDateChange} />
+				</label>
+				<label>
+					Location:{" "}
+					<input type="text" id="location" onChange={handleLocationChange} />
+				</label>
+			</form>
+
+			<button
+				onClick={() =>
+					setExperience(experience.filter((exp) => exp.id !== experienceId))
+				}
+			>
+				DELETE
+			</button>
+		</li>
+	);
+}
