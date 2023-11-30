a5d4da7818119bac67582d4dcecf5892b0b7020c
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2023-11-30 14:49:51 +0300

    Style general info for preview section

--- src/GeneralPreview.jsx
+++ src/GeneralPreview.jsx
@@ -2,9 +2,11 @@ export default function GeneralPreview({ generalInfo }) {
 	return (
 		<div className="general-info">
 			<h1>{generalInfo.firstName + " " + generalInfo.lastName}</h1>
-			<span className="address">{generalInfo.address}</span>
-			<span className="email">{generalInfo.email}</span>
-			<span className="phone">{generalInfo.phone}</span>
+			<div>
+				<span className="address">{generalInfo.address}</span>
+				<span className="email">{generalInfo.email}</span>
+				<span className="phone">{generalInfo.phone}</span>
+			</div>
 		</div>
 	);
 }
