c097cf01ab8f8476d88ba4870c52016b8ecb9c30
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2023-11-30 15:57:40 +0300

    Fix footer not appeating at the bottom of the page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import CVGenerator from "./CVGenerator";
+import Footer from "./Footer";
 
 function App() {
 	return (
 		<>
 			<Header />
 			<CVGenerator />
+			<Footer />
 		</>
 	);
 }
