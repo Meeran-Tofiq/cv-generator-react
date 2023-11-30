45ecc6bc55dc2823de97e55d914582d1c985254c
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2023-11-30 15:46:16 +0300

    Feat credits and link to my github page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+	return (
+		<footer>
+			Made by{" "}
+			<a href="https://github.com/Meeran-Tofiq">
+				Meeran Tofiq <i class="fa-brands fa-github" />
+			</a>
+			;
+		</footer>
+	);
+}
